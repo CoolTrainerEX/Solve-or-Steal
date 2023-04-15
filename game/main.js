efee4adaf276e414a10f43ecba7b1cf06e933c50
CoolTrainerEX <cooltrainerex@gmail.com>
2023-04-15 11:35:47 +0800

    rges

--- game/main.js
+++ game/main.js
@@ -1,17 +1,5 @@
-function next() {
-    newQuestion();
-    resetTimer();
-}
-
-function steal(element) {
-    resetTimer();
-}
-
-function answer(ans) {
-    (ans == correct) ? correct : wrong;
-}
-
-function newQuestion() {
-    removeUsedQuestion();
-    getRandomQuestion();
+var players = [document.getElementById("bottom"), document.getElementById("left"), document.getElementById("top"), document.getElementById("right")];
+var question = document.getElementById("screen").children[0]
+function nextQuestion() {
+    katex.render("\\approx 69", question);
 }
