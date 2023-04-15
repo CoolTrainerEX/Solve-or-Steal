1aa3c1ac179fd5149867f31d7b99254deb35e1cf
CoolTrainerEX <cooltrainerex@gmail.com>
2023-04-15 12:34:15 +0800

    ghf

--- game/main.js
+++ game/main.js
@@ -1,5 +1,5 @@
 var players = [document.getElementById("bottom"), document.getElementById("left"), document.getElementById("top"), document.getElementById("right")];
-var question = document.getElementById("screen").children[0]
+var question = document.getElementById("screen").children[0];
 function nextQuestion() {
-    katex.render("\\frac {2x^2 - 8x + 8} {2x - 4} \\text { continuous at } [0, 1)", question);
+
 }
