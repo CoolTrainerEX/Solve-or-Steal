ee88f90c1fe0a7a05a494ab3c9195536beea3da9
CoolTrainerEX <cooltrainerex@gmail.com>
2023-04-16 11:23:13 +0800

    AEg

--- game/rotate.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("top").lastElementChild.setAttribute("transform", "rotate(180)");
-document.getElementById("left").lastElementChild.setAttribute("transform", "rotate(90)");
-document.getElementById("right").lastElementChild.setAttribute("transform", "rotate(-90)");
